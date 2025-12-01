cc33dd40f7b0ddaaf4f0ba2f653d9c6fd49e23c8
Mike <michelsanchezaldana1@gmail.com>
2025-12-01 05:33:47 -0400

    Add delete_student  to student tests

--- App/tests/test_student.py
+++ App/tests/test_student.py
@@ -1,7 +1,7 @@
 import pytest
 from App.main import create_app
 from App.database import db
-from App.controllers.student_controller import register_student
+from App.controllers.student_controller import register_student, delete_student
 from App.models import Student
 
 @pytest.fixture(autouse=True)
@@ -19,3 +19,10 @@ def test_register_student():
     assert student.username == "Alice"
     assert student.student_id is not None
     assert str(student.student_id).startswith("8160")
+
+def test_delete_student():
+    student = register_student("Bob", "bob@example.com", "pw")
+    sid = student.student_id
+    result = delete_student(sid)
+    assert result is True
+    assert Student.query.get(sid) is None
