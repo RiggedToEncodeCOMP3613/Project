f9e5d234d8722554642ec22e08b25c24eb6d7e94
Pubblez <isabellafluffz@gmail.com>
2025-12-03 03:54:05 -0400

    CSS added in style.css and blueprints added in __init__.py so routes can be made in the view student_page.py

--- App/views/__init__.py
+++ App/views/__init__.py
@@ -5,8 +5,9 @@ from .index import index_views
 from .auth import auth_views
 from .admin import setup_admin
 from .student import student_views
+from .student_page import student_page_views
 from .staff import staff_views
 
 
-views = [user_views, index_views, auth_views, student_views, staff_views] 
+views = [user_views, index_views, auth_views, student_views, staff_views, student_page_views] 
 # blueprints must be added to this list
