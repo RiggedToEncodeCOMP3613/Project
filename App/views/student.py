d3b94c6ede254011f75e3b5f578b44358395e5a4
NeedRam <rajaropen@gmail.com>
2025-12-03 03:47:18 +0000

    added student main menu

--- App/views/student.py
+++ App/views/student.py
@@ -1,10 +1,72 @@
 from flask import Blueprint, render_template, jsonify, request, send_from_directory, flash, redirect, url_for
 from flask_jwt_extended import jwt_required, current_user as jwt_current_user
-from App.models import Student
+from App.models import Student, RequestHistory
 from.index import index_views
 from App.controllers.student_controller import get_all_students_json,fetch_accolades,create_hours_request
 
 student_views = Blueprint('student_views', __name__, template_folder='../templates')
+
+@student_views.route('/student/main', methods=['GET'])
+@jwt_required()
+def student_main_menu():
+    user = jwt_current_user
+    if user.role != 'student':
+        flash('Access forbidden: Not a student')
+        return redirect('/login')
+
+    student = Student.query.get(user.student_id)
+    if not student:
+        flash('Student profile not found')
+        return redirect('/login')
+
+    # Get stats
+    total_hours = student.total_hours
+    pending_requests = RequestHistory.query.filter_by(student_id=user.student_id, status='Pending').count()
+    milestones_count = len(student.check_for_milestones())
+    accolades_count = len(student.check_accolades())
+
+    return render_template('student_main_menu.html',
+                         student=student,
+                         total_hours=total_hours,
+                         pending_requests=pending_requests,
+                         milestones_count=milestones_count,
+                         accolades_count=accolades_count)
+
+@student_views.route('/student/make-request', methods=['GET'])
+@jwt_required()
+def student_make_request():
+    user = jwt_current_user
+    if user.role != 'student':
+        flash('Access forbidden: Not a student')
+        return redirect('/login')
+    return render_template('message.html', title="Make Request", message="Make Request page - Coming Soon!")
+
+@student_views.route('/student/stats', methods=['GET'])
+@jwt_required()
+def student_stats():
+    user = jwt_current_user
+    if user.role != 'student':
+        flash('Access forbidden: Not a student')
+        return redirect('/login')
+    return render_template('message.html', title="View Stats", message="View Stats page - Coming Soon!")
+
+@student_views.route('/student/profile', methods=['GET'])
+@jwt_required()
+def student_profile():
+    user = jwt_current_user
+    if user.role != 'student':
+        flash('Access forbidden: Not a student')
+        return redirect('/login')
+    return render_template('message.html', title="Profile", message="Profile page - Coming Soon!")
+
+@student_views.route('/student/leaderboard', methods=['GET'])
+@jwt_required()
+def student_leaderboard():
+    user = jwt_current_user
+    if user.role != 'student':
+        flash('Access forbidden: Not a student')
+        return redirect('/login')
+    return render_template('message.html', title="Leaderboard", message="Leaderboard page - Coming Soon!")
 
 @student_views.route('/api/accolades', methods=['GET'])
 @jwt_required()
