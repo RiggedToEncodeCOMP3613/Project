84ce0334011e0ffe882da3b0f21e59bbfe93d52b
NeedRam <rajaropen@gmail.com>
2025-12-03 22:23:56 +0000

    hopefully the deployment works now

--- App/config.py
+++ App/config.py
@@ -6,6 +6,14 @@ def load_config(app, overrides):
     else:
         app.config.from_object('App.default_config')
     app.config.from_prefixed_env()
+    # Set database URI for production (Render)
+    if os.getenv('ENV') == 'production':
+        db_user = os.getenv('POSTGRES_USER')
+        db_password = os.getenv('POSTGRES_PASSWORD')
+        db_host = os.getenv('POSTGRES_URL')
+        db_name = os.getenv('POSTGRES_DB')
+        if db_user and db_password and db_host and db_name:
+            app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{db_user}:{db_password}@{db_host}/{db_name}'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.config['PREFERRED_URL_SCHEME'] = 'https'
